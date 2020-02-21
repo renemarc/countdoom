ba15682b6e4fe729cca36677cc71d05be2e492f5
renemarc <renemarc@gmail.com>
2020-02-20 19:01:40 -0500

    chore(tox): expand tox environments

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="Fetch the current Doomsday Clock from TheBulletin.org",
     entry_points={
